1f34e81bbe7a99d3979b52620ba26241eed600c7
Fatima Ez-zahraa <fatimaezzahraa.aithssain@gmail.com>
2024-09-05 21:19:06 +0100

    add HeaderBox component

---
+++ fullstack/app/(root)/page.tsx
@@ -0,0 +1,23 @@
+import HeaderBox from '@/components/HeaderBox'
+import React from 'react'
+
+const Home = () => {
+  const loggedIn = { firstName: "Fatima Ez-Zahraa"}
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title="Welcome" //needs to be included in the translation
+          user={loggedIn?.firstName || "Guest"} //needs to be included in the translation
+          subtext= "Access and manage your account and transactions efficiently" //needs to be included in the translation
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ fullstack/components/HeaderBox.tsx
@@ -0,0 +1,8 @@
+
+const HeaderBox = ({type = "title", title, user, subtext }: HeaderBoxProps) => {
+  return (
+    <div>HeaderBox</div>
+  )
+}
+
+export default HeaderBox
