8a73a0f760e68e912a11ea81e101998f86e75e00
Fatima Ez-zahraa <fatimaezzahraa.aithssain@gmail.com>
2024-09-05 22:54:41 +0100

    create new components

--- fullstack/app/(root)/page.tsx
+++ fullstack/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
@@ -9,11 +10,16 @@ const Home = () => {
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox 
-          type="greeting"
-          title="Welcome" //needs to be included in the translation
-          user={loggedIn?.firstName || "Guest"} //needs to be included in the translation
-          subtext= "Access and manage your account and transactions efficiently" //needs to be included in the translation
+            type="greeting"
+            title="Welcome" //needs to be included in the translation
+            user={loggedIn?.firstName || "Guest"} //needs to be included in the translation
+            subtext= "Access and manage your account and transactions efficiently" //needs to be included in the translation
           />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+           />
         </header>
       </div>
     </section>
